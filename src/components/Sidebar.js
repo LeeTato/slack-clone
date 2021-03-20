6cade9f097338014c099c07e9dda9191eaa83a2e
Lielt <tab271621@gmail.com>
2021-03-20 00:23:55 -0500

    Sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,20 @@
 // import { styled } from '@material-ui/core';
 import styled from "styled-components";
-import React from 'react'
+import React from 'react';
 import CreateIcon from '@material-ui/icons/Create';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import SidebarOption from './SidebarOption';
+import InsertCommentIcon from '@material-ui/icons/InsertComment';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import AppsIcon from '@material-ui/icons/Apps';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add';
+
 
 
 function Sidebar() {
@@ -19,7 +31,21 @@ function Sidebar() {
           <CreateIcon/>
        
         </SidebarHeader>
+        <SidebarOption Icon={InsertCommentIcon} title="Threads"/>
+        <SidebarOption Icon={InboxIcon} title="Mentions & reactions"/>
+        <SidebarOption Icon={DraftsIcon} title="Saved items"/>
+        <SidebarOption Icon={BookmarkBorderIcon} title="Channel browser"/>
+        <SidebarOption Icon={PeopleAltIcon} title="People & user groups"/>
+        <SidebarOption Icon={AppsIcon} title="File browser"/>
+        <SidebarOption Icon={FileCopyIcon} title="File browser"/>
+        <SidebarOption Icon={ExpandLessIcon} title="Show less"/>
+        <hr />
+        <SidebarOption Icon={ExpandMoreIcon} title="Channels"/>
+        <hr />
+        <SidebarOption Icon={AddIcon} addChannelOption  title="Add Channel"/>
     </SidebarContainer>
+  
+
     );
 }
 
@@ -61,5 +87,11 @@ margin-bottom: 5px;
     font-weight: 400;
     align-items: center;
 }
+ > h3> .MuiSvgIcon-root{
+   font-size: 14px;
+   margin-top:1px;
+   margin-right:2px;
+   color:green;
+ }
 `;
 
